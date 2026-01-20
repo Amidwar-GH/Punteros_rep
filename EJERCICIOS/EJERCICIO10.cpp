6397c270b1551abc0055285d1ae04fa79b809430
Amidwar <amidwar05@gmail.com>
2026-01-19 22:09:35 -0500

    Terminando el ejercicio de matriz transpuesta

--- EJERCICIOS/EJERCICIO10.cpp
+++ EJERCICIOS/EJERCICIO10.cpp
@@ -4,12 +4,20 @@ using namespace std;
 
 void pedirDatos();
 void transpuesta(int **, int **, int, int);
+void mostrarMatriz(int **, int, int);
 
 
 int **puntero_matriz1, **puntero_matriz2, nFilas,nCol;
 
 int main(){
     pedirDatos();
+    cout<<"\nLa matriz original es:\n";
+    mostrarMatriz(puntero_matriz1, nFilas, nCol);
+
+    transpuesta(puntero_matriz1,puntero_matriz2,nFilas,nCol);
+
+    cout<<"\nLa matriz transpuesta es: \n";
+    mostrarMatriz(puntero_matriz2, nCol, nFilas);
 
     system("pause");
     return 0;
@@ -36,11 +44,26 @@ void pedirDatos(){
     }
 
     //Reservando memorira para la segunda matriz
-    puntero_matriz2 = new int*[nFilas]; //Reservando memoria para las filas
-    for(int i=0; i<nFilas; i++){
-        puntero_matriz2[i] = new int[nCol];
+    puntero_matriz2 = new int*[nCol]; //Reservando memoria para las filas
+    for(int i=0; i<nCol; i++){
+        puntero_matriz2[i] = new int[nFilas];
     }
 
 }
 
+void transpuesta(int **puntero_matriz1, int **puntero_matriz2, int nFilas, int nCol){
+    for(int i=0; i<nFilas; i++){
+        for(int j=0; j<nCol; j++){
+            *(*(puntero_matriz2+j)+i)=*(*(puntero_matriz1+i)+j);
+        }
+    }
+}
 
+void mostrarMatriz(int **matriz,int nFilas, int nCol){
+    for(int i=0; i<nFilas; i++){
+        for(int j=0; j<nCol; j++){
+            cout<<*(*(matriz+i)+j)<<" ";
+        }
+        cout<<endl;
+    }
+}
